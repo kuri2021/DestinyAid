ed56b814b594d64ec43f28b003339ca0fd95fbdc
kuri2021 <oring1998@naver.com>
2021-03-17 16:50:47 +0900

    21.03.17

--- app/src/main/java/com/Destiny_Aid/destinyaid/Free_Board.java
+++ app/src/main/java/com/Destiny_Aid/destinyaid/Free_Board.java
@@ -1,5 +1,8 @@
 package com.Destiny_Aid.destinyaid;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +21,29 @@ public class Free_Board extends Fragment {
     ArrayList<BoardItems> items=new ArrayList<BoardItems>();
     RecyclerView recyclerView;
     BoardAdapter adapter;
-
-    TextView tv;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.free_board,container,false);
         recyclerView=view.findViewById(R.id.recycler);
-        adapter=new BoardAdapter(getActivity().getApplicationContext());
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            String[] permissions={Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            if(getActivity().checkSelfPermission(permissions[0])== PackageManager.PERMISSION_DENIED){
+                requestPermissions(permissions, 100);
+            }
+        }
+        return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==100 && grantResults[0]==PackageManager.PERMISSION_DENIED){
+
+        }
+    }
+    public void clickSelect(View view){
+
     }
 }

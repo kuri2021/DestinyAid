e82674c4672a83dd79e8c23619b1721149228b68
kuri2021 <oring1998@naver.com>
2021-04-12 09:54:55 +0900

    21.04.12

--- app/src/main/java/com/Destiny_Aid/destinyaid/Collection_Wonder.java
+++ app/src/main/java/com/Destiny_Aid/destinyaid/Collection_Wonder.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import java.io.BufferedInputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class Collection_Wonder extends AppCompatActivity {
@@ -29,6 +34,57 @@ public class Collection_Wonder extends AppCompatActivity {
 
         
 
+
+        items.add(new Collection_wonder_Item("가시","핸드캐논"
+                ,R.drawable.thon
+                ,R.drawable.kinds
+                ,R.drawable.nasun
+                ,R.drawable.accurized
+                ,R.drawable.gost
+                ,R.drawable.textured_grip));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+        items.add(new Collection_wonder_Item("루미나","핸드캐논"
+                ,R.drawable.lumina
+                ,R.drawable.kinds
+                ,R.drawable.sil
+                ,R.drawable.accurized
+                ,R.drawable.sky
+                ,R.drawable.gobunja));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+        items.add(new Collection_wonder_Item("매의 달","핸드캐논"
+                ,R.drawable.horus
+                ,R.drawable.kinds
+                ,R.drawable.nasun
+                ,R.drawable.gapgum
+                ,R.drawable.notting
+                ,R.drawable.soft));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+        items.add(new Collection_wonder_Item("매의 달","핸드캐논"
+                ,R.drawable.bubuq
+                ,R.drawable.kinds
+                ,R.drawable.nasun
+                ,R.drawable.yunjang
+                ,R.drawable.gulboc
+                ,R.drawable.gobunja));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+        items.add(new Collection_wonder_Item("스페이드 에이스","핸드캐논"
+                ,R.drawable.spaceace
+                ,R.drawable.kinds
+                ,R.drawable.nasun
+                ,R.drawable.big
+                ,R.drawable.fire
+                ,R.drawable.soft));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
         items.add(new Collection_wonder_Item("유언","핸드캐논"
                 ,R.drawable.lastword_small
                 ,R.drawable.kinds
@@ -39,7 +95,16 @@ public class Collection_Wonder extends AppCompatActivity {
         recyclerView=findViewById(R.id.recycler_collection_wonder);
         adapter= new Collection_wonder_Adapter(this,items);
         recyclerView.setAdapter(adapter);
-
+        items.add(new Collection_wonder_Item("진홍 핏자국","핸드캐논"
+                ,R.drawable.gingong
+                ,R.drawable.kinds
+                ,R.drawable.sogugung
+                ,R.drawable.accurized
+                ,R.drawable.heal
+                ,R.drawable.hip_fire_grip));
+        recyclerView=findViewById(R.id.recycler_collection_wonder);
+        adapter= new Collection_wonder_Adapter(this,items);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
